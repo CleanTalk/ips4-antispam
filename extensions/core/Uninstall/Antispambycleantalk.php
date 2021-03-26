cfed4b000a41528788a4b1fb240e20267d18edef
Artem Davydov <yozzeebeatz@gmail.com>
2021-03-26 14:18:50 +0500

    Add files.

---
+++ extensions/core/Uninstall/Antispambycleantalk.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @brief		Uninstall callback
+ * @author		<a href='https://www.invisioncommunity.com'>Invision Power Services, Inc.</a>
+ * @copyright	(c) Invision Power Services, Inc.
+ * @license		https://www.invisioncommunity.com/legal/standards/
+ * @package		Invision Community
+ * @subpackage	Antispam by Cleantalk
+ * @since		02 Nov 2020
+ */
+
+namespace IPS\antispambycleantalk\extensions\core\Uninstall;
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	header( ( isset( $_SERVER['SERVER_PROTOCOL'] ) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0' ) . ' 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Uninstall callback
+ */
+class _antispambycleantalk
+{
+	/**
+	 * Code to execute before the application has been uninstalled
+	 *
+	 * @param	string	$application	Application directory
+	 * @return	array
+	 */
+	public function preUninstall( $application )
+	{
+	}
+
+	/**
+	 * Code to execute after the application has been uninstalled
+	 *
+	 * @param	string	$application	Application directory
+	 * @return	array
+	 */
+	public function postUninstall( $application )
+	{
+	}
+
+	/**
+	 * Code to execute when other applications are uninstalled
+	 *
+	 * @param	string	$application	Application directory
+	 * @return	void
+	 * @deprecated	This is here for backwards-compatibility - all new code should go in onOtherUninstall
+	 */
+	public function onOtherAppUninstall( $application )
+	{
+		return $this->onOtherUninstall( $application );
+	}
+
+	/**
+	 * Code to execute when other applications or plugins are uninstalled
+	 *
+	 * @param	string	$application	Application directory
+	 * @param	int		$plugin			Plugin ID
+	 * @return	void
+	 */
+	public function onOtherUninstall( $application=NULL, $plugin=NULL )
+	{
+	}
+}
